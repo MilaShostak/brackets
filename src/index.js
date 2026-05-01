53e33fa2db7f52760c7da284a15b36a757c2da67
MilaShostak <milashostak111@gmail.com>
2026-05-01 14:58:59 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+  const bracketsMap = Object.fromEntries(
+    bracketsConfig.map(([open, close]) => [close, open])
+  );
+
+  const openBrackets = bracketsConfig.map((pair) => pair[0]);
+
+  for (let i = 0; i < str.length; i += 1) {
+    const char = str[i];
+    const topElement = stack[stack.length - 1];
+
+    if (char === bracketsMap[char] && openBrackets.includes(char)) {
+      if (topElement === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (bracketsMap[char]) {
+      if (stack.pop() !== bracketsMap[char]) {
+        return false;
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.length === 0;
 };
